6bd9aa71ee816be93583c5f44e9321493855d565
gogi999 <g.petrovic9@gmail.com>
2022-09-18 20:14:54 +0200

    Chat app completed

--- src/src/components/Input.jsx
+++ src/src/components/Input.jsx
@@ -75,25 +75,25 @@ const Input = () => {
   };
   return (
     <div className="input">
+      <input
+        type="text"
+        placeholder="Type something..."
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
+      <div className="send">
+        <img src={Attach} alt="" />
         <input
-            type="text"
-            placeholder="Type something..."
-            onChange={(e) => setText(e.target.value)}
-            value={text}
+          type="file"
+          style={{ display: "none" }}
+          id="file"
+          onChange={(e) => setImg(e.target.files[0])}
         />
-        <div className="send">
-            <img src={Attach} alt="" />
-            <input
-                type="file"
-                style={{ display: "none" }}
-                id="file"
-                onChange={(e) => setImg(e.target.files[0])}
-            />
-            <label htmlFor="file">
-                <img src={Img} alt="" />
-            </label>
-            <button onClick={handleSend}>Send</button>
-        </div>
+        <label htmlFor="file">
+          <img src={Img} alt="" />
+        </label>
+        <button onClick={handleSend}>Send</button>
+      </div>
     </div>
   );
 };
--- src/src/firebase.js
+++ src/src/firebase.js
@@ -4,13 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBszEIvrxkiKc0snte-1ReG1gT2GUJO0QI",
-    authDomain: "chat-2fb6c.firebaseapp.com",
-    projectId: "chat-2fb6c",
-    storageBucket: "chat-2fb6c.appspot.com",
-    messagingSenderId: "986967222445",
-    appId: "1:986967222445:web:c93dbd69e51828d5c5fe24"
-}
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "chat-ab746.firebaseapp.com",
+  projectId: "chat-ab746",
+  storageBucket: "chat-ab746.appspot.com",
+  messagingSenderId: "901216368405",
+  appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
